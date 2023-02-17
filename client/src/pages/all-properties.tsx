fe0289873b85d475d3c3dc399077807c5f6ad732
Ksenija <ksenija.cvjetan@gmail.com>
2023-02-18 00:13:09 +0100

    Dodati kosturi za sortiranje, pretragu i selekciju

--- client/src/pages/all-properties.tsx
+++ client/src/pages/all-properties.tsx
@@ -2,7 +2,7 @@ import { Add } from "@mui/icons-material";
 
 import { useTable } from "@pankod/refine-core";
 
-import { Typography, Box, Stack} from '@pankod/refine-mui';
+import { Typography, Box, Stack, TextField, Select, MenuItem} from '@pankod/refine-mui';
 
 import { useNavigate } from "@pankod/refine-react-router-v6";
 
@@ -13,7 +13,13 @@ const AllProperties = () => {
   const navigate = useNavigate();
 
   const {
-    tableQueryResult: {data, isLoading, isError}
+    tableQueryResult: {data, isLoading, isError},
+    current,
+    setCurrent,
+    setPageSize,
+    pageCount,
+    sorter, setSorter,
+    filters, setFilters,
   } = useTable();
 
   const AllProperties = data?.data ?? [];
@@ -23,12 +29,57 @@ const AllProperties = () => {
 
   return (
     <Box>
+        <Box mt ="20px" sx={{display:"flex", flexWrap:"wrap", gap:3}}>
+          <Stack direction="column" width="100%">
+            <Typography fontSize={25} fontWeight={700} color="#11142d">
+              {!AllProperties.length ? 'There are no properties' : 'All Properties'}</Typography>
+            <Box mb={2} mt={3} display='flex' width='84%' justifyContent="space-between" flexWrap="wrap">
+                <Box display="flex" gap={2} flexWrap='wrap' marginBottom={{xs:'20px', sm: 0}}>
+                    <CustomButton
+                      title = {'Sort price'}
+                      handleClick={() => {}}
+                      backgroundColor="#475be8"
+                      color="#fcfcfc"
+                    />
+
+                    <TextField
+                      variant="outlined"
+                      color="info"
+                      placeholder="Search by title"
+                      value=''
+                      onChange={() => {}}
+                    />
+
+                    <Select
+                      variant="outlined"
+                      color="info"
+                      displayEmpty
+                      required
+                      inputProps={{'aria-label': 'Without label'}}
+                      defaultValue=""
+                      value=""
+                      onChange={() => {}}
+                    >
+
+                      <MenuItem value="">All</MenuItem>
+
+                    </Select>
+                </Box>
+            </Box>
+          </Stack>
+        </Box>
+
+
+
+
+
+
       <Stack
         direction="row"
         justifyContent="space-between"
         alignItems="center"
       >
-        <Typography fontSize={25} fontWeight={700} color="#11142d">All Properties</Typography>
+        
         <CustomButton
           title="Add Property"
           handleClick={() => navigate('/properties/create')}
