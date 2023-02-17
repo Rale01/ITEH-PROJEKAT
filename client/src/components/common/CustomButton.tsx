b441575729bab750d867fda396a5349d0403d3b0
Ksenija <ksenija.cvjetan@gmail.com>
2023-02-18 00:29:57 +0100

    Kreiran i kostur za paginaciju

--- client/src/components/common/CustomButton.tsx
+++ client/src/components/common/CustomButton.tsx
@@ -4,9 +4,10 @@ import { CustomButtonProps } from "interfaces/common";
 
 
 
-const CustomButton = ({type, title, backgroundColor, color, fullWidth, icon, handleClick}: CustomButtonProps) => {
+const CustomButton = ({type, title, backgroundColor, color, fullWidth, icon, handleClick, disabled}: CustomButtonProps) => {
   return (
     <Button
+      disabled={disabled}
       type = {type === 'submit' ? 'submit' : 'button'}
       sx={{
         flex: fullWidth ? 1 : 'unset',
